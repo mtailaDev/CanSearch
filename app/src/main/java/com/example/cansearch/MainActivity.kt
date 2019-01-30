8d73fbb1a478a432bf073aaec783b3ff9fc8375e
MTailaCodes <mtailacodes@gmail.com>
2019-01-29 23:23:45 -0500

    Added functionality to skip onboarding and login

--- app/src/main/java/com/example/cansearch/MainActivity.kt
+++ app/src/main/java/com/example/cansearch/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.cansearch
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.cansearch.home.HomeActivity
 import com.example.cansearch.login.LoginFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +15,9 @@ class MainActivity : AppCompatActivity() {
         val ft = supportFragmentManager.beginTransaction()
         ft.replace(R.id.fragment, LoginFragment())
         ft.commit()
+
+        login_tv_skip.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/cansearch/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.cansearch.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cansearch.R
+
+class HomeActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
