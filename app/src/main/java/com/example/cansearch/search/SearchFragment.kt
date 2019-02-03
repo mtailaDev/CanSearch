84b4ccbd51786bdb837a4a666457c804369aa3ac
MTailaCodes <mtailacodes@gmail.com>
2019-02-03 12:58:02 -0500

    Setup Quick Search RecyclerView

--- app/src/main/java/com/example/cansearch/search/SearchFragment.kt
+++ app/src/main/java/com/example/cansearch/search/SearchFragment.kt
@@ -5,12 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cansearch.R
+import com.example.cansearch.search.ui.QuickSearchAdapter
+import kotlinx.android.synthetic.main.fragment_search.*
 
-class SearchFragment : Fragment(){
+class SearchFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupQuickSearchRecyclerView()
+    }
+
+    private fun setupQuickSearchRecyclerView() {
+        val stringArray = resources.getStringArray(R.array.quickSearch)
+        search_rv_quick_search.adapter = QuickSearchAdapter(stringArray.toList())
+        search_rv_quick_search.layoutManager = LinearLayoutManager(context)
+    }
 }
---
+++ app/src/main/java/com/example/cansearch/search/ui/QuickSearchAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.cansearch.search.ui
+
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cansearch.R
+import kotlinx.android.synthetic.main.item_list_quick_search.view.*
+
+
+class QuickSearchAdapter(val quickSearchList: List<String>) : RecyclerView.Adapter<QuickSearchAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuickSearchAdapter.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val quickSearchView = inflater.inflate(R.layout.item_list_quick_search, parent, false)
+        return ViewHolder(quickSearchView)
+    }
+
+    override fun onBindViewHolder(viewHolder: QuickSearchAdapter.ViewHolder, position: Int) {
+        viewHolder.setQuickSearch(quickSearchList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return quickSearchList.size
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun setQuickSearch(quickSearchTitle: String) {
+            itemView.quick_search_title.text = quickSearchTitle
+        }
+    }
+}
