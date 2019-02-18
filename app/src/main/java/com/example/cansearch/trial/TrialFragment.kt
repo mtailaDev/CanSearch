fce1ff95a84969f990b6c90db12dd0ffa46e7f13
MTailaCodes <mtailacodes@gmail.com>
2019-02-17 23:22:46 -0500

    Added base logic/ui/implementation to show trial summary container

--- app/src/main/java/com/example/cansearch/trial/TrialFragment.kt
+++ app/src/main/java/com/example/cansearch/trial/TrialFragment.kt
@@ -5,9 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cansearch.R
+import kotlinx.android.synthetic.main.fragment_trial.*
+import kotlinx.android.synthetic.main.trials_summary_compound.*
 
 class TrialFragment : Fragment() {
+
+    private val trialSummaryList = ArrayList<TrialSummaryItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +24,22 @@ class TrialFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        generateStubData()
+    }
+
+    private fun setupRecyclerView() {
+        trial_summary.setSummaryRecyclerView(generateStubData())
+    }
+
+    private fun generateStubData(): ArrayList<TrialSummaryItem> {
+        trialSummaryList.clear()
+        trialSummaryList.add(TrialSummaryItem("Principle Investigator", "Dr. Matt Taila", false))
+        trialSummaryList.add(TrialSummaryItem("Lead Organization", "TGen", false))
+        trialSummaryList.add(TrialSummaryItem("Phase", "III", true))
+        trialSummaryList.add(TrialSummaryItem("Activity Status", "Active", true))
+        trialSummaryList.add(TrialSummaryItem("Primary Purpose", "Treatment", true))
+        trialSummaryList.add(TrialSummaryItem("Anatomic Site", "Breast - female", false))
+        return trialSummaryList
     }
 }
---
+++ app/src/main/java/com/example/cansearch/trial/TrialSummaryItem.kt
@@ -0,0 +1,3 @@
+package com.example.cansearch.trial
+
+data class TrialSummaryItem(val summaryTitle: String, val summaryValue: String, val highlight: Boolean)
