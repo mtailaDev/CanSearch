38832acc2a31f6d16b04809331f71f478b3956ce
MTailaCodes <mtailacodes@gmail.com>
2019-02-19 22:34:25 -0500

    Added logic/ui/ and basic setup for eigibility cardview

---
+++ app/src/main/java/com/example/cansearch/trial/EligibilityCompoundView.kt
@@ -0,0 +1,37 @@
+package com.example.cansearch.trial
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.cansearch.R
+import kotlinx.android.synthetic.main.eligibility_compound.view.*
+
+class EligibilityCompoundView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) :
+    ConstraintLayout(context, attrs, defStyleAttr) {
+
+    init {
+        inflate(context, R.layout.eligibility_compound, this)
+        setEligibilityRecyclerView()
+    }
+
+    fun setEligibilityRecyclerView() {
+        // todo
+//        val eligibilityAdapter = TrialEligibilityAdapter(list)
+        val eligibilityAdapter = TrialEligibilityAdapter(generateStubData())
+        eligibility_recycler_view.adapter = eligibilityAdapter
+        eligibility_recycler_view.layoutManager = LinearLayoutManager(context)
+    }
+
+    private fun generateStubData(): List<TrialEligibility> {
+        val eligibilityList = mutableListOf<TrialEligibility>()
+        eligibilityList.add(TrialEligibility("Sex", "Both"))
+        eligibilityList.add(TrialEligibility("Minimum Age", "18"))
+        eligibilityList.add(TrialEligibility("Maximum Age", "--"))
+        return eligibilityList
+    }
+}
---
+++ app/src/main/java/com/example/cansearch/trial/TrialEligibility.kt
@@ -0,0 +1,3 @@
+package com.example.cansearch.trial
+
+data class TrialEligibility(val eligibilityTitle: String, val eligibilityValue: String)
