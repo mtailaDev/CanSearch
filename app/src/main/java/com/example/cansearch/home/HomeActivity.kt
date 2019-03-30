6798d31967573c69b46c9f50f10ad4329897b11b
Matt Taila <mtailadev@gmail.com>
2019-03-30 17:42:30 -0400

    Deleted / commented out previous navigational logic

--- app/src/main/java/com/example/cansearch/home/HomeActivity.kt
+++ app/src/main/java/com/example/cansearch/home/HomeActivity.kt
@@ -1,26 +1,13 @@
 package com.example.cansearch.home
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cansearch.R
-import com.example.cansearch.search.domain.SearchScreen
-import com.example.cansearch.search.ui.screens.SearchFragment
-import com.example.cansearch.search.ui.screens.TrialActivity
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        val ft = supportFragmentManager.beginTransaction()
-        ft.replace(R.id.fragment, SearchFragment())
-        ft.commit()
-    }
-
-    fun showTrial(selectedTrial: SearchScreen.SearchResult) {
-        val intent = Intent(this, TrialActivity::class.java)
-        intent.putExtra("Test", selectedTrial.id)
-        startActivity(intent)
     }
 }
