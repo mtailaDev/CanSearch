52ce1e1beca536e6623258805520fd4a985db320
Matt Taila <mtailadev@gmail.com>
2019-03-31 14:05:46 -0400

    Setup home navigation to placeholder fragments for archivedTrials and settings page

--- app/src/main/java/com/example/cansearch/home/HomeActivity.kt
+++ app/src/main/java/com/example/cansearch/home/HomeActivity.kt
@@ -2,12 +2,20 @@ package com.example.cansearch.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
 import com.example.cansearch.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setNavClickListener()
+    }
+
+    private fun setNavClickListener() {
+        home_bottom_nav.setupWithNavController(Navigation.findNavController(this, R.id.nav_host_fragment))
     }
 }
