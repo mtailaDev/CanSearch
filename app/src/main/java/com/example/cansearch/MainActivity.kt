b4e38ed24ec7bf156fb130ee690ac5ef5ab56ca4
Matt Taila <mtailadev@gmail.com>
2019-03-30 23:47:51 -0400

    Cleared Main Activity nav logic

--- app/src/main/java/com/example/cansearch/MainActivity.kt
+++ app/src/main/java/com/example/cansearch/MainActivity.kt
@@ -1,24 +1,13 @@
 package com.example.cansearch
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.cansearch.home.HomeActivity
-import com.example.cansearch.login.LoginFragment
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val ft = supportFragmentManager.beginTransaction()
-        ft.replace(R.id.fragment, LoginFragment())
-        ft.commit()
-
-        login_tv_skip.setOnClickListener {
-            startActivity(Intent(this, HomeActivity::class.java))
-        }
     }
 
 }
